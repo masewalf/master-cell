40694e39f15ce26cd0019486bea5ff73fffd0c37
masewalf <masewalf3@gmail.com>
2022-03-28 14:01:51 -0400

    mi primer commit

---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/style.css">
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Red+Hat+Mono:ital,wght@0,300;0,400;1,700&display=swap" rel="stylesheet">
+    <title>MasterCell</title>
+</head>
+<body>
+    <header class="header" id="inicio">
+        <img src="images/hamburguersa.svg" alt="" class="hamburguer">
+        <nav class="menu-navegacion">
+    <a href="#inicio">Inicio</a>
+    <a href="#servicio">Servicio</a>
+    <a href="#portafolio">Portafolio</a>
+    <a href="#expertos">Expertos</a>
+    <a href="#contacto">Contacto</a>
+</nav>  
+    <div class="contenedor head">
+    <h1 class="titulo">convertimos tus suenos en realidad</h1>
+     <p class="copy">Damos servicios de soporte técnico puntual o por iguala. On Site o Remoto.
+        Ayudamos a las empresas y negocios, gerentes y emprendedores, empleados y colaboradores, a funcionar mejor por medio de la tecnología...
+        >Implementando mejores sistemas y equipos.
+    </p>
+    </div>
+</header>
+<main>
+    <section class="contenedor" id="servicio">
+        <h2 class="subtitulo">Nuestro servicio</h2>
+        <div class="contenedor-servicio">
+            <img src="images/checklist.svg"alt="">
+            <div class="checklist-servicio">
+                <div class="service">
+                    <h3 class="n-service"><span class="number">1</span>Soporte tecnico remoto</h3>
+                     <p> Trabbajamos remotamente sin necesidad de traer tu pc con Anydesk, Teamviewer, para corregir los problemas que le presente su equipo. </p>
+            </div>
+            <div class="service">
+                <h3 class="n-service"><span class="number">2</span>Soporte tenico a domilio</h3>
+                <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius quisquam sed ex sunt illum quia veritatis totam distinctio modi tempora!</p>
+            </div>
+            <div class="service">
+                <h3 class="n-service"><span class="number">3</span>Instalacion de redes estructuradas</h3>
+                <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius quisquam sed ex sunt illum quia veritatis totam distinctio modi tempora!</p>
+            </div>           
+        </div>
+    </div>
+    </section>`
+    <section class="gallery" id="portafolio">
+        <div class="contenedor">
+            <h2 class="subtitulo">Galeria</h2>
+            <div class="contenedor-galeria">
+                <img src="images/foto1.jpg" alt="" class="img-galeria">
+                <img src="images/foto2.jpg" alt="" class="img-galeria">
+                <img src="images/foto3.jpg" alt="" class="img-galeria">
+                <img src="images/foto4.jpg" alt="" class="img-galeria">
+                <img src="images/foto5.jpg" alt="" class="img-galeria">
+                <img src="images/foto6.jpg" alt="" class="img-galeria">
+            </div>
+        </div>
+    </section>
+        <section class="imagen-light">
+        <img src="images/close.svg" alt="" class="close">
+        <img src="images/foto9.jpg" alt="" class="agregar-imagen">
+    </section>
+
+    <section class="contenedor" id="expertos">
+        <h2 class="subtitulo">Expertos en:</h2>
+        <Section class="experts">
+            <div class="cont-expert">
+                <img src="images/Phone maintenance_Flatline.svg"alt="">
+                <h3 class="n-expert">Celulares</h3>
+            </div>
+            <div class="cont-expert">
+                <img src="images/World wide web_Monochromatic.svg" alt="">
+                <h3 class="n-expert">REDES</h3>
+            </div>
+            <div class="cont-expert">
+                <img src="images/Cleaning PC_Monochromatic.svg" alt="">
+                <h3 class="n-expert">Computadoras</h3>
+            </div>
+        </Section>
+    </section>
+
+    </main>
+    <footer id="contacto">
+        <div class="contenedor footer-content">
+            <div class="contact-us">
+                <h2 class="brand">MasterCell</h2>
+                <p>Somos expertos en tecnologia</p>
+            </div>
+            <div class="social-media">
+                 <a href="./" class="social-media-icon">
+                <i class='bx bxl-whatsapp'></i>
+            </a>
+            <a href="./" class="social-media-icon">
+                <i class='bx bxl-instagram'></i>
+            </a>
+            <a href="./" class="social-media-icon">
+                <i class='bx bxl-facebook-circle'></i>
+            </a>
+        </div>
+    </div>
+    <div class="line"</div>
+</footer>
+  
+<script src="js/menu.js"></script>
+<script src="js/lightbox.js"></script>
+
+</body>
+</html>
